1e5a47460c483efb6e1ad0a255fdb7b98cf19d47
AKASH <AKASH@DESKTOP-IM6L1R0>
2022-03-29 14:54:58 +0530

    aa

--- com.crm.cheeteh/src/test/java/com/crm/cheeteh/VtigerTest.java
+++ com.crm.cheeteh/src/test/java/com/crm/cheeteh/VtigerTest.java
@@ -27,33 +27,33 @@ public class VtigerTest {
 		  System.out.println(PASSWORD);
 		 
 		
-//	if  ( BROWSER.equals("chrome")) {
-//			WebDriverManager.chromedriver().setup();
-//		  driver = new ChromeDriver();
-//			}
-//		else if (BROWSER.equals("firefox")) {
-//			WebDriverManager.firefoxdriver().setup();
-//			driver=new FirefoxDriver();
-//		}
-//		else {
-//			throw new Exception("browser is not compatible");
-//		}
-//		
-//		
-//		
-//		driver.manage().window().maximize();
-//		driver.get(URl);
-//		
-//		driver.findElement(By.name("user_name")).sendKeys(USERNAME);
-//		driver.findElement(By.name("user_password")).sendKeys(PASSWORD);
-//		driver.findElement(By.id("submitButton")).click();
-//		
-//		
-//		Actions act = new Actions(driver);
-//	WebElement administrator =	driver.findElement(By.xpath("//img[@src='themes/softed/images/user.PNG']"));
-//	act.moveToElement(administrator).perform();
-//		driver.findElement(By.linkText("Sign Out")).click();
-//		
+	if  ( BROWSER.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+		  driver = new ChromeDriver();
+			}
+		else if (BROWSER.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+		}
+		else {
+			throw new Exception("browser is not compatible");
+		}
+		
+		
+		
+		driver.manage().window().maximize();
+		driver.get(URl);
+		
+		driver.findElement(By.name("user_name")).sendKeys(USERNAME);
+		driver.findElement(By.name("user_password")).sendKeys(PASSWORD);
+		driver.findElement(By.id("submitButton")).click();
+		
+		
+		Actions act = new Actions(driver);
+	WebElement administrator =	driver.findElement(By.xpath("//img[@src='themes/softed/images/user.PNG']"));
+	act.moveToElement(administrator).perform();
+		driver.findElement(By.linkText("Sign Out")).click();
+		
 		
 		
 	}
